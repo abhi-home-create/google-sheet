1fba2d4a0b5cde0054179dac1899550b6a579470
Abhishek <yadavabhi0143@gmail.com>
2025-01-17 12:37:32 +0530

    Update build.js

--- build.js
+++ build.js
@@ -1,5 +1,13 @@
 const fs = require('fs');
 
-const script = fs.readFileSync('script.js', 'utf8');
-const updatedScript = script.replace('GOOGLE_APPS_SCRIPT_URL', process.env.GOOGLE_APPS_SCRIPT_URL);
-fs.writeFileSync('dist/script.js', updatedScript);
+// Create config file with environment variable
+const configContent = `const CONFIG = {
+    SCRIPT_URL: '${process.env.GOOGLE_APPS_SCRIPT_URL}'
+};`;
+
+fs.writeFileSync('dist/config.js', configContent);
+
+// Copy other files
+fs.copyFileSync('index.html', 'dist/index.html');
+fs.copyFileSync('styles.css', 'dist/styles.css');
+fs.copyFileSync('script.js', 'dist/script.js');
